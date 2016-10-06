bd041669092267eba373f5a2ad814c96fcc1f864
Petar Velichkov <petio.kv@gmail.com>
2016-10-06 16:37:42 +0300

    Delete printLetters.js

--- JS Fundamentals/Homework/Strings and Regular Expressions/printLetters.js
+++
@@ -1,6 +0,0 @@
-function printLetters(input) {
-    for (let i = 0; i < input[0].length; i++) {
-        console.log(`str[${i}] -> ${input[0].charAt(i)}`);
-    }
-}
-printLetters(['Hello, World!']);
