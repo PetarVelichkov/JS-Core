5be5a83fdda28e785a4314fbc233a296410fe1f4
Petar Velichkov <petio.kv@gmail.com>
2016-10-06 16:32:50 +0300

    Delete repeatStringNTimes.js

--- JS Fundamentals/Homework/Strings and Regular Expressions - LAB/repeatStringNTimes.js
+++
@@ -1,9 +0,0 @@
-function repeatString([text, repeat]) {
-    repeat = Number(repeat);
-    let result = text.repeat(repeat);
-    console.log(result);
-}
-repeatString(['magic is real','5']);
-/*let f = ([string, times]) => string.repeat(Number(times));
-
- console.log(f(['magic is real', '3']));*/
