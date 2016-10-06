002bd2c496da703410e95ed079a21c41e16805a4
Petar Velichkov <petio.kv@gmail.com>
2016-10-06 16:30:42 +0300

    Delete findVariableNamesInSentences.js

--- JS Fundamentals/Homework/Strings and Regular Expressions - LAB/findVariableNamesInSentences.js
+++
@@ -1,9 +0,0 @@
-function findVariables([text]) {
-    let regex = /\b_([a-zA-Z0-9]+)\b/g;
-    let words = [], match;
-    while (match = regex.exec(text)) {
-        words.push(match[1]);
-    }
-    console.log(words.join(','));
-}
-findVariables(['The _id and _age variables are both integers.']);
